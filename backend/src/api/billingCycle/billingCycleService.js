d8f7d71a49d452b883c18227451b9e0599e20777
juniorcampaneli <junior.campaneli@gmail.com>
2022-12-27 10:09:21 -0300

    v1

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,44 @@
+const billingCycle = require('./billingCycle');
+const errorHandler = require('../common/errorHandler')
+
+billingCycle.methods(['get', 'post', 'put', 'delete'])
+billingCycle.updateOptions({new: true, runValidators: true})
+billingCycle.after('post', errorHandler).after('put', errorHandler)
+
+billingCycle.route('get', (req, res, next)=>{
+    billingCycle.find({}, (err, docs)=>{
+        if(!err){
+            res.json(docs)
+        }else{
+            res.status(500).json({errors: [err]})
+        }
+    })
+})
+
+billingCycle.route('count', (req, res, next)=>{
+    billingCycle.count((error, value)=>{
+        if(error){
+            res.status(500).json({errors:[error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
+billingCycle.route('summary', (req,res,next)=>{
+    billingCycle.aggregate([{
+        $project: {credit:{$sum: "$credits.value"}, debt:{$sum: "$debts.value"}}
+    },{
+        $group:{_id:null, credit: {$sum:"$credit"}, debt: {$sum:"$debt"}}
+    },{
+        $project: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) =>{
+        if(error){
+            res.status(500).json({errros: [error]})
+        }else{
+            res.json(result[0] || {credit:0, debt:0})
+        }
+    })
+})
+
+module.exports = billingCycle
